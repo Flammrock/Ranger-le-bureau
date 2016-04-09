8e2aae153c5516a0042f5abeb5629f5fc0b41489
Flammrock <flammrockgame@gmail.com>
2016-04-09 13:24:32 +0200

    Added files via upload

---
+++ bureau.bat
@@ -0,0 +1,25 @@
+﻿@echo off
+set /a NUM = 0
+:: se place sur le bureau
+cd %USERPROFILE%\Desktop
+
+:: Récupére le chemin courant
+set currentpath1=%~dp0
+:: Récupére le chemin du bureau
+set currentpath=%USERPROFILE%\Desktop\
+
+
+if not exist %currentpath1%rapport.txt echo //RAPPORT > %currentpath1%rapport.txt
+
+
+call %currentpath1%Extension\PNG.bat
+call %currentpath1%Extension\JPG.bat
+call %currentpath1%Extension\BMP.bat
+call %currentpath1%Extension\TIF.bat
+call %currentpath1%Extension\MP4.bat
+call %currentpath1%Extension\MKV.bat
+call %currentpath1%Extension\AVI.bat
+call %currentpath1%Extension\MP3.bat
+call %currentpath1%Extension\WAV.bat
+call %currentpath1%Extension\OGG.bat
+call %currentpath1%Extension\OGG.bat
